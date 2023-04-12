ac0fc3a5426c550e47e585144126ea2d6d03de06
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-04-12 18:52:14 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -39,11 +39,11 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
         await query.message.edit_text(
             text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
+                first = query.message.from_user.first_name,
+                last = query.message.from_user.last_name,
+                username = None if not query.message.from_user.username else '@' + message.from_user.username,
+                mention = query.message.from_user.mention,
+                id = query.message.from_user.id
             ),
             reply_markup = reply_markup,
             disable_web_page_preview = True,
